02de4e33d67e55095efafe3a701aceab56cdc44a
Maxim Kucherenko <maximkucherenkowork@gmail.com>
2022-06-12 12:05:34 +0300

    Add CORS

--- index.js
+++ index.js
@@ -1,29 +1,33 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const authRouter = require('./authRouter')
-const mainRouter = require('./mainRouter')
-const profileRouter = require('./profileRouter')
-const PORT = process.env.PORT || 5000
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const authRouter = require("./authRouter");
+const mainRouter = require("./mainRouter");
+const profileRouter = require("./profileRouter");
+const PORT = process.env.PORT || 5000;
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.use("/auth", authRouter)
-app.use("/profile", profileRouter)
-app.use("/main", mainRouter)
+app.use(express.json());
+app.use(cors());
+app.use("/auth", authRouter);
+app.use("/profile", profileRouter);
+app.use("/main", mainRouter);
 
-mongoose.set('useNewUrlParser', true);
-mongoose.set('useFindAndModify', false);
-mongoose.set('useCreateIndex', true);
+mongoose.set("useNewUrlParser", true);
+mongoose.set("useFindAndModify", false);
+mongoose.set("useCreateIndex", true);
 
 const start = async () => {
-    try {
-        await mongoose.connect(`mongodb+srv://Admin:admin@cluster0.qqxj2.mongodb.net/?retryWrites=true&w=majority`, {useNewUrlParser: true, useUnifiedTopology: true})
-        app.listen(PORT, () => console.log(`server started on port ${PORT}`))
-    } catch (e) {
-        console.log(e)
-    }
-}
+  try {
+    await mongoose.connect(
+      `mongodb+srv://Admin:admin@cluster0.qqxj2.mongodb.net/?retryWrites=true&w=majority`,
+      { useNewUrlParser: true, useUnifiedTopology: true }
+    );
+    app.listen(PORT, () => console.log(`server started on port ${PORT}`));
+  } catch (e) {
+    console.log(e);
+  }
+};
 
-start()
-
+start();
