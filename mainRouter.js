9af88698ad9957d4cc1cef3d2121c98bc3dfd89a
Maxim Kucherenko <maximkucherenkowork@gmail.com>
2022-06-15 18:20:10 +0300

    Change search method to POST

--- mainRouter.js
+++ mainRouter.js
@@ -1,13 +1,13 @@
-const Router = require('express');
+const Router = require("express");
 const router = new Router();
-const controller = require('./authController');
-const authMiddleware = require('./middlewaree/authMiddleware');
+const controller = require("./authController");
+const authMiddleware = require("./middlewaree/authMiddleware");
 
-const User = require('./models/User');
+const User = require("./models/User");
 
-router.get('/users', authMiddleware, controller.getUsers);
+router.get("/users", authMiddleware, controller.getUsers);
 
-router.get('/user/:username', authMiddleware, async (req, res) => {
+router.get("/user/:username", authMiddleware, async (req, res) => {
   try {
     const username = req.params.username;
     const user = await User.findOne({ username: username });
@@ -17,7 +17,7 @@ router.get('/user/:username', authMiddleware, async (req, res) => {
   }
 });
 
-router.get('/user', authMiddleware, async (req, res) => {
+router.get("/user", authMiddleware, async (req, res) => {
   try {
     const user = await User.findOne({ _id: req.user.id });
     res.json(user);
@@ -26,11 +26,11 @@ router.get('/user', authMiddleware, async (req, res) => {
   }
 });
 
-router.get('/search', authMiddleware, async (req, res) => {
+router.post("/search", authMiddleware, async (req, res) => {
   try {
     const search = req.body.search;
     const users = await User.find({
-      interests: { $regex: search, $options: 'i' }
+      interests: { $regex: search, $options: "i" },
     });
     res.json(users);
   } catch (e) {
